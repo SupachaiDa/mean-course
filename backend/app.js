05689dc25622d08fe8c2ac39b966ff9dca4b0253
Mark Spc <SupachaiDa>
2019-08-13 21:01:36 +0700

    connect to mongodb database

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const Post = require('./models/post')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+mongoose.connect('mongodb+srv://Mark:Mark123@cluster0-q6zto.mongodb.net/mean-course?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to database')
+  })
+  .catch((() => {
+    console.log('Fail to connect to database')
+  }))
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); //allow all domain to access
@@ -20,23 +31,33 @@ app.use((req, res, next) => {
 
 
 app.post('/api/posts',(req,res) => {
-  const post = req.body;
-  console.log(post)
-  res.status(201).json({
-    message: 'Added post successfully',
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
   })
+  post.save().then( cratedPost => {
+    res.status(201).json({
+    message: 'Added post successfully',
+    postId: cratedPost._id //ส่ง postId ให้ postService ที่จากเดิมเป็น null
+  })})
 })
 
-app.use('/api/posts', (req,res) => {
-  posts = [
-    {id:'sd665645' ,title:'First Post' ,content:'testing 1'},
-    {id:'frsf5455' ,title:'Second Post' ,content:'testing 2'},
-    {id:'ssadc545' ,title:'Third Post' ,content:'testing 3'}
-  ]
-  res.status(200).json({
+app.get('/api/posts', (req,res) => {
+  Post.find()
+    .then( documents => {
+    res.status(200).json({
     message: 'fetch data success',
-    posts: posts
+    posts: documents
   })
+    })
+})
+
+app.delete('/api/posts/:id', (req,res) => {
+  Post.deleteOne({_id: req.params.id})
+    .then( result => {
+      console.log(result)
+      res.status(200).json({message: 'Post deleted'})
+    } )
 })
 
 module.exports = app
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -13,24 +14,43 @@ export class PostsService {
     constructor( private http: HttpClient) {}
 
     getPost() {
-        this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-          .subscribe((postsData) => {
-            this.posts = postsData.posts;
+        this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+        // convert id to _id
+     /*pipe is a method that can add multiple operator */ .pipe(map((postData) => {
+            return postData.posts.map((post) => {
+              return {
+                id: post._id,
+                title: post.title,
+                content: post.content
+              };
+            });
+          } ))
+          .subscribe((transformPosts) => {
+            this.posts = transformPosts;
             this.postUpdated.next([...this.posts]);
           });
     }
-
     postUpdatedListener() {
      return  this.postUpdated.asObservable(); // make posts can listen from outside
     }
 
     addpost(title: string , content: string) {
         const post: Post = { id: null , title: title , content: content}
-        this.http.post<{ message: string }>('http://localhost:3000/api/posts' , post)
-          .subscribe((responData) => {
-            console.log(responData.message);
+        this.http.post<{ message: string , postId: string}>('http://localhost:3000/api/posts' , post)
+          .subscribe((responseData) => {
+            const id  = responseData.postId;
+            post.id = id;
             this.posts.push(post);
             this.postUpdated.next([...this.posts]); // emit posts
           });
+    }
+
+    deletePost(postId: string) {
+      this.http.delete('http://localhost:3000/api/posts/' + postId)
+        .subscribe(() => {
+          const updatedPostAfterDelete = this.posts.filter( post => post.id !== postId);
+          this.posts = updatedPostAfterDelete;
+          this.postUpdated.next([...this.posts]);
+        });
     }
 }
